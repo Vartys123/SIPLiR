34c4b1535a4cf825f29362076de7a253c1e82e40
Vartys <143608183+Vartys123@users.noreply.github.com>
2026-04-14 22:42:40 +0300

    0.0.2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::read_to_string;
+
 #[derive(Debug)]
 enum Token {
     Num(i32),
@@ -8,12 +10,23 @@ enum Token {
     Identifier(String),
 }
 
+impl Token {
+    fn deref(self) -> i32 {
+        match self {
+            Token::Num(n) => n,
+            Token::Add(n1, n2) => n1.deref() + n2.deref(),
+            Token::Sub(n1, n2) => n1.deref() - n2.deref(),
+            _ => unimplemented!(),
+        }
+    }
+}
+
 fn lex(code: &str) -> Vec<Token> {
     code.split_whitespace()
         .filter_map(|word| match word {
             "+" => Some(Token::AddOp),
             "-" => Some(Token::SubOp),
-            n => n.parse::<i32>().ok().map(|n| Token::Num(n)),
+            n => n.parse::<i32>().ok().map(Token::Num),
         })
         .collect()
 }
@@ -41,8 +54,16 @@ fn parse(lexed: Vec<Token>) -> Vec<Token> {
     tokens
 }
 
+fn run(parsed: Vec<Token>) {
+    for token in parsed {
+        println!("{:?}", token.deref());
+    }
+}
+
 fn main() {
-    let tokens = lex(include_str!("../something.lg"));
+    let tokens = lex(&read_to_string("something.lg").expect("Failed to read the file"));
     println!("Lexed: {:?}", tokens);
-    println!("Parsed: {:?}", parse(tokens));
+    let tokens = parse(tokens);
+    println!("Parsed: {:?}", tokens);
+    run(tokens);
 }
