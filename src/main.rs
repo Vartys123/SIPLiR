12fb7933c925a31d5d3d0f9efd572b84234aefca
Vartys <143608183+Vartys123@users.noreply.github.com>
2026-04-28 16:57:13 +0300

    0.0.6
    Replaced panics with error handling.
    Added underscore support and explicit whitespace skipping to the lexer.
    Added division-by-zero check and EOF handling.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,20 @@ use std::fs::read_to_string;
 use std::iter::Peekable;
 
 #[derive(Debug)]
+enum ErrorKind {
+    LexerError(&'static str),
+    ParserError(&'static str),
+    EvalError(&'static str),
+    IoError(std::io::Error),
+}
+
+impl From<std::io::Error> for ErrorKind {
+    fn from(err: std::io::Error) -> Self {
+        ErrorKind::IoError(err)
+    }
+}
+
+#[derive(Debug)]
 enum Token<'a> {
     Num(i32),
     AddOp,
@@ -49,12 +63,14 @@ fn get_bp(token: &Token) -> u8 {
     }
 }
 
-fn lex(input: &str) -> Vec<Token> {
+fn lex(input: &str) -> (Vec<Token>, Vec<ErrorKind>) {
     let bytes = input.as_bytes();
     let mut tokens = Vec::new();
+    let mut errors = Vec::new();
     let mut i = 0;
     while i < bytes.len() {
         match bytes[i] {
+            b' ' | b'\n' | b'\t' | b'\r' => i += 1,
             b'+' => {
                 tokens.push(Token::AddOp);
                 i += 1
@@ -88,38 +104,50 @@ fn lex(input: &str) -> Vec<Token> {
                 while i < bytes.len() && bytes[i].is_ascii_digit() {
                     i += 1
                 }
-                let n = input[start..i].parse().expect("Error!!!!!!");
-                tokens.push(Token::Num(n));
+                match input[start..i].parse() {
+                    Ok(n) => tokens.push(Token::Num(n)),
+                    Err(..) => errors.push(ErrorKind::LexerError("Failed to parse integer!")),
+                }
             }
-            b'a'..=b'z' | b'A'..=b'Z' => {
+            b'a'..=b'z' | b'A'..=b'Z' | b'_' => {
                 let start = i;
-                while i < bytes.len() && bytes[i].is_ascii_alphanumeric() {
+                while i < bytes.len() && (bytes[i].is_ascii_alphanumeric() || bytes[i] == b'_') {
                     i += 1
                 }
                 tokens.push(Token::Identifier(&input[start..i]));
             }
-            _ => i += 1,
+            _ => {
+                errors.push(ErrorKind::LexerError("Unrecognized character!"));
+                i += 1;
+            }
         }
     }
     tokens.push(Token::EOF);
-    tokens
+    (tokens, errors)
 }
 
-fn parse_expr<'a>(iter: &mut Peekable<impl Iterator<Item = Token<'a>>>, bp_start: u8) -> Expr<'a> {
-    let first_token = iter.next().expect("Error!!!!!!");
+fn parse_expr<'a>(
+    iter: &mut Peekable<impl Iterator<Item = Token<'a>>>,
+    bp_start: u8,
+) -> Result<Expr<'a>, ErrorKind> {
+    let first_token = iter
+        .next()
+        .ok_or(ErrorKind::ParserError("Unexpected End Of Sequence!"))?;
     let mut left = match first_token {
         Token::Num(n) => Expr::Num(n),
         Token::Identifier(s) => Expr::Identifier(s),
         Token::LParen => {
-            let inner = parse_expr(iter, 0);
-            iter.next().expect("Error!!!!!!");
-            inner
+            let inner = parse_expr(iter, 0)?;
+            match iter.next() {
+                Some(Token::RParen) => inner,
+                _ => return Err(ErrorKind::ParserError("Unclosed Parenthesis!")),
+            }
         }
         Token::SubOp => {
-            let inner = parse_expr(iter, 25);
+            let inner = parse_expr(iter, 25)?;
             Expr::Unary(UnaryOp::Negative, Box::new(inner))
         }
-        _ => unimplemented!(),
+        _ => return Err(ErrorKind::ParserError("Unexpected first token!")),
     };
 
     while let Some(next_token) = iter.peek() {
@@ -129,8 +157,8 @@ fn parse_expr<'a>(iter: &mut Peekable<impl Iterator<Item = Token<'a>>>, bp_start
         }
         let op = iter.next().unwrap();
         let right = match op {
-            Token::PowOp => parse_expr(iter, bp - 1),
-            _ => parse_expr(iter, bp),
+            Token::PowOp => parse_expr(iter, bp - 1)?,
+            _ => parse_expr(iter, bp)?,
         };
         let opkind = match op {
             Token::AddOp => BinaryOp::Add,
@@ -138,43 +166,68 @@ fn parse_expr<'a>(iter: &mut Peekable<impl Iterator<Item = Token<'a>>>, bp_start
             Token::MulOp => BinaryOp::Mul,
             Token::DivOp => BinaryOp::Div,
             Token::PowOp => BinaryOp::Pow,
-            _ => unimplemented!(),
+            _ => return Err(ErrorKind::ParserError("Unexpected operation!")),
         };
         left = Expr::Binary(opkind, Box::new(left), Box::new(right));
     }
-    left
+    Ok(left)
 }
 
-fn parse(lexed: Vec<Token>) -> Expr {
+fn parse(lexed: Vec<Token>) -> Result<Expr, ErrorKind> {
     let mut iter = lexed.into_iter().peekable();
-    parse_expr(&mut iter, 0)
-}
-
-fn eval(parsed: &Expr) -> i32 {
-    match parsed {
-        Expr::Num(n) => *n,
-        Expr::Unary(kind, inner) => match kind {
-            UnaryOp::Positive => eval(inner),
-            UnaryOp::Negative => -eval(inner),
-            _ => unimplemented!(),
-        },
-        Expr::Binary(kind, left, right) => match kind {
-            BinaryOp::Add => eval(left) + eval(right),
-            BinaryOp::Sub => eval(left) - eval(right),
-            BinaryOp::Mul => eval(left) * eval(right),
-            BinaryOp::Div => eval(left) / eval(right),
-            BinaryOp::Pow => eval(left).pow(eval(right).try_into().expect("Error!!!!!!")),
-            _ => unimplemented!(),
-        },
-        _ => unimplemented!(),
+    let expr = parse_expr(&mut iter, 0)?;
+    match iter.next() {
+        Some(Token::EOF) => Ok(expr),
+        _ => Err(ErrorKind::ParserError("Unexpected trailing characters!")),
     }
 }
 
-fn main() {
-    let file_content = read_to_string("something.lg").expect("Failed to read the file");
-    let tokens = lex(&file_content);
+fn eval(parsed: &Expr) -> Result<i32, ErrorKind> {
+    Ok(match parsed {
+        Expr::Num(n) => *n,
+        Expr::Unary(kind, inner) => match kind {
+            UnaryOp::Positive => eval(inner)?,
+            UnaryOp::Negative => -eval(inner)?,
+            _ => return Err(ErrorKind::EvalError("Unexpected unary operation!")),
+        },
+        Expr::Binary(kind, left, right) => {
+            let l = eval(left)?;
+            let r = eval(right)?;
+            match kind {
+                BinaryOp::Add => l + r,
+                BinaryOp::Sub => l - r,
+                BinaryOp::Mul => l * r,
+                BinaryOp::Div => {
+                    if r == 0 {
+                        return Err(ErrorKind::EvalError("Division by zero!"));
+                    }
+                    l / r
+                }
+                BinaryOp::Pow => l.pow(
+                    r.try_into()
+                        .map_err(|_| ErrorKind::EvalError("Failed to convert the type!"))?,
+                ),
+                _ => return Err(ErrorKind::EvalError("Unexpected binary operation!")),
+            }
+        }
+        _ => return Err(ErrorKind::EvalError("Not implemented!")),
+    })
+}
+
+fn main() -> Result<(), ErrorKind> {
+    let file_content = read_to_string("something.lg")?;
+
+    let (tokens, errors) = lex(&file_content);
+    if !errors.is_empty() {
+        for error in errors {
+            println!("{:?}", error);
+        }
+        return Ok(());
+    }
     println!("Lexed: {:?}", tokens);
-    let tokens = parse(tokens);
+    let tokens = parse(tokens)?;
     println!("Parsed: {:?}", tokens);
-    println!("Result: {:?}", eval(&tokens));
+    println!("Result: {:?}", eval(&tokens)?);
+
+    Ok(())
 }
